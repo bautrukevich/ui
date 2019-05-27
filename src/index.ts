e695811d67db30da17988b77ca130e5025b894ca
Siarhei Bautrukevich <s.boltrukevich@gmail.com>
2019-05-27 21:14:46 +0800

    Export Select & TextArea components

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import {Input} from './base/Input/Input'
 import {Link} from './base/Link/Link'
+import {Select} from './base/Select/Select'
+import {TextArea} from './base/TextArea/TextArea'
 
 export {
   Input,
   Link,
+  Select,
+  TextArea,
 }
