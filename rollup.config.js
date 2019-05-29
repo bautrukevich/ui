407452a946a05a05f2e9bdf1ceb04371c1f47e6a
Siarhei Bautrukevich <s.boltrukevich@gmail.com>
2019-05-29 09:07:39 +0800

    Add some rules and fix lint errors

--- rollup.config.js
+++ rollup.config.js
@@ -4,16 +4,16 @@ import typescript from 'rollup-plugin-typescript';
 import pkg from './package.json';
 
 export default [
-	// browser-friendly UMD build
-	{
-		input: 'src/index.ts',
-		output: {
-			name: pkg.name,
-			file: pkg.browser,
-			format: 'umd'
-		},
-		plugins: [
-			resolve(),   // so Rollup can find deps
+  // browser-friendly UMD build
+  {
+    input: 'src/index.ts',
+    output: {
+      name: pkg.name,
+      file: pkg.browser,
+      format: 'umd'
+    },
+    plugins: [
+      resolve(),   // so Rollup can find deps
       commonjs({
         include: 'node_modules/**',
         // namedExports: {
@@ -31,24 +31,24 @@ export default [
         //   ]
         // }
       }),  // so Rollup can convert deps to an ES module
-			typescript() // so Rollup can convert TypeScript to JavaScript
-		]
-	},
+      typescript() // so Rollup can convert TypeScript to JavaScript
+    ]
+  },
 
-	// CommonJS (for Node) and ES module (for bundlers) build.
-	// (We could have three entries in the configuration array
-	// instead of two, but it's quicker to generate multiple
-	// builds from a single configuration where possible, using
-	// an array for the `output` option, where we can specify
-	// `file` and `format` for each target)
-	{
-		input: 'src/index.ts',
-		external: ['react', 'react-dom', 'styled-components'],
-		plugins: [
-			typescript() // so Rollup can convert TypeScript to JavaScript
-		],
-		output: [
-			{ file: pkg.module, format: 'es' }
-		]
-	}
+  // CommonJS (for Node) and ES module (for bundlers) build.
+  // (We could have three entries in the configuration array
+  // instead of two, but it's quicker to generate multiple
+  // builds from a single configuration where possible, using
+  // an array for the `output` option, where we can specify
+  // `file` and `format` for each target)
+  {
+    input: 'src/index.ts',
+    external: ['react', 'react-dom', 'styled-components'],
+    plugins: [
+      typescript() // so Rollup can convert TypeScript to JavaScript
+    ],
+    output: [
+      { file: pkg.module, format: 'es' }
+    ]
+  }
 ];
